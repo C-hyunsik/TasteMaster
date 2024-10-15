b886f3af26a9ab54fccdd3972332392a1a03fda7
unknown <j3415879@naver.com>
2024-10-15 21:10:31 +0900

    ncp serivce추가

--- src/main/java/comment/service/impl/CommentServiceImpl.java
+++ src/main/java/comment/service/impl/CommentServiceImpl.java
@@ -4,10 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import comment.dao.CommentDAO;
+import comment.service.CommentService;
 import dish.service.DishService;
 
 @Service
-public class CommentServiceImpl implements DishService {
+public class CommentServiceImpl implements CommentService {
 	@Autowired
 	private CommentDAO commentDAO;
 
--- src/main/java/post/service/impl/PostServiceImpl.java
+++ src/main/java/post/service/impl/PostServiceImpl.java
@@ -5,9 +5,10 @@ import org.springframework.stereotype.Service;
 
 import dish.service.DishService;
 import post.dao.PostDAO;
+import post.service.PostService;
 
 @Service
-public class PostServiceImpl implements DishService {
+public class PostServiceImpl implements PostService {
 	@Autowired
 	private PostDAO postDAO;
 
