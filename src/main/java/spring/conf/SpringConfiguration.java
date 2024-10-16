d97f3c423c154314adf7c64fce4d0c943b842aa3
gustlr887 <gustlr887@naver.com>
2024-10-16 10:19:29 +0900

    Mapper 설정 수정

--- src/main/java/spring/conf/SpringConfiguration.java
+++ src/main/java/spring/conf/SpringConfiguration.java
@@ -4,7 +4,10 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -15,11 +18,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 @PropertySource("classpath:spring/db.properties")
+@MapperScan("chef.dao")
 public class SpringConfiguration {
 	private @Value("${jdbc.driver}") String driver;
 	private @Value("${jdbc.url}") String url;
 	private @Value("${jdbc.username}") String username;
 	private @Value("${jdbc.password}") String password;
+	
+	@Autowired
+    private ApplicationContext context;
 	
 	@Bean
 	public BasicDataSource dataSource() {
@@ -36,9 +43,9 @@ public class SpringConfiguration {
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setDataSource(dataSource());
 		sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("spring/mybatis-config.xml"));
-		sqlSessionFactoryBean.setMapperLocations(new ClassPathResource("mapper/memberMapper.xml"));
-		sqlSessionFactoryBean.setMapperLocations(new ClassPathResource("mapper/chefMapper.xml"));
-		sqlSessionFactoryBean.setMapperLocations(new ClassPathResource("mapper/dishMapper.xml"));
+		sqlSessionFactoryBean.setMapperLocations(
+    			context.getResources("classpath:mapper/*Mapper.xml"));
+		
 		return sqlSessionFactoryBean.getObject(); // SqlSessionFactory 변환 역할
 	}
 	
