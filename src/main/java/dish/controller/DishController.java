974180cf05a1b27cf168b0b2118c58bca0103b81
unknown <j3415879@naver.com>
2024-10-17 11:51:15 +0900

    d

--- src/main/java/dish/controller/DishController.java
+++ src/main/java/dish/controller/DishController.java
@@ -5,13 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-=======
-import org.springframework.web.bind.annotation.RequestMapping;
->>>>>>> 5fa3749c47eba32b6377cf019fb812eb633a7aa3
 
 import dish.bean.DishDTO;
 import dish.service.DishService;
@@ -22,7 +18,7 @@ public class DishController {
 	@Autowired
 	private DishService dishService;
 
-<<<<<<< HEAD
+
     @RequestMapping(value = "/page/dish/dishList")
   	public String pageChefDishLists(@RequestParam String chefId, Model model) throws IOException {
     	
@@ -32,10 +28,5 @@ public class DishController {
   	}
     
     
-=======
-	@RequestMapping(value="/page/dish/dishlist")
-	public String pageDishDishList() {
-		return "/dish/dishList";
-	}
->>>>>>> 5fa3749c47eba32b6377cf019fb812eb633a7aa3
+
 }
